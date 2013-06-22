4a888e31884678ebb33592ac68ee5cb0f18fddd6
Fernando Paredes <nanoid.xd@gmail.com>
2013-06-22 10:42:28 -0700

    Move Reggy to the Cask directory

--- reggy.rb
+++
@@ -1,7 +0,0 @@
-class Reggy < Cask
-  url 'http://github.com/downloads/samsouder/reggy/Reggy_v1.3.tbz'
-  homepage 'http://reggyapp.com/'
-  version '1.3'
-  sha1 '13f822dffb8ae189d1e312c1e94144d656063411'
-  link 'Reggy.app'
-end
