Homebrew/homebrew-cask

Add Audio Unit and VST plugin folder artifiacts

lib/hbc/artifact.rb
@@ -13,6 +13,8 @@ require 'hbc/artifact/font'
 require 'hbc/artifact/input_method'
 require 'hbc/artifact/installer'
 require 'hbc/artifact/internet_plugin'
+require 'hbc/artifact/audiounit_plugin'
+require 'hbc/artifact/vst_plugin'
 require 'hbc/artifact/nested_container'
 require 'hbc/artifact/pkg'
 require 'hbc/artifact/postflight_block'
@@ -49,6 +51,8 @@ module Hbc::Artifact
       Hbc::Artifact::Binary,
       Hbc::Artifact::InputMethod,
       Hbc::Artifact::InternetPlugin,
+      Hbc::Artifact::AudioUnitPlugin,
+      Hbc::Artifact::VSTPlugin,
       Hbc::Artifact::ScreenSaver,
       Hbc::Artifact::Uninstall,
       Hbc::Artifact::PostflightBlock,

lib/hbc/cli.rb
@@ -179,6 +179,12 @@ class Hbc::CLI
       opts.on("--internet_plugindir=MANDATORY") do |v|
         Hbc.internet_plugindir = Pathname(v).expand_path
       end
+      opts.on("--audiounit_plugindir=MANDATORY") do |v|
+        Hbc.audiounit_plugindir = Pathname(v).expand_path
+      end
+      opts.on("--vst_plugindir=MANDATORY") do |v|
+        Hbc.vst_plugindir = Pathname(v).expand_path
+      end
       opts.on("--screen_saverdir=MANDATORY") do |v|
        Hbc.screen_saverdir = Pathname(v).expand_path
       end

dasbaumwolltier/homebrew-unsigned-casks

lib/hbc/artifact.rb
@@ -13,6 +13,8 @@ require 'hbc/artifact/font'
 require 'hbc/artifact/input_method'
 require 'hbc/artifact/installer'
 require 'hbc/artifact/internet_plugin'
+require 'hbc/artifact/audio_unit_plugin'
+require 'hbc/artifact/vst_plugin'
 require 'hbc/artifact/nested_container'
 require 'hbc/artifact/pkg'
 require 'hbc/artifact/postflight_block'
@@ -49,6 +51,8 @@ module Hbc::Artifact
       Hbc::Artifact::Binary,
       Hbc::Artifact::InputMethod,
       Hbc::Artifact::InternetPlugin,
+      Hbc::Artifact::AudioUnitPlugin,
+      Hbc::Artifact::VstPlugin,
       Hbc::Artifact::ScreenSaver,
       Hbc::Artifact::Uninstall,
       Hbc::Artifact::PostflightBlock,

lib/hbc/cli.rb
@@ -179,6 +179,12 @@ class Hbc::CLI
       opts.on("--internet_plugindir=MANDATORY") do |v|
         Hbc.internet_plugindir = Pathname(v).expand_path
       end
+      opts.on("--audio_unit_plugindir=MANDATORY") do |v|
+        Hbc.audio_unit_plugindir = Pathname(v).expand_path
+      end
+      opts.on("--vst_plugindir=MANDATORY") do |v|
+        Hbc.vst_plugindir = Pathname(v).expand_path
+      end
       opts.on("--screen_saverdir=MANDATORY") do |v|
        Hbc.screen_saverdir = Pathname(v).expand_path
       end