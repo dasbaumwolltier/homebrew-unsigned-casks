Homebrew/homebrew-cask

Fixed the cli to include --skip-cask-deps to skip all casks dependencies

test/cask/cli/install_test.rb
@@ -42,6 +42,16 @@ describe Hbc::CLI::Install do
     }, %r{==> Success! local-transmission staged at '#{Hbc.caskroom}/local-transmission/2.61' \(487 files, 11M\)})
   end
 
+  it "skip dependencies with --skip-cask-deps" do
+    shutup do
+      Hbc::CLI::Install.run('with-depends-on-cask-multiple', '--skip-cask-deps')
+    end
+    Hbc.load('with-depends-on-cask-multiple').must_be :installed?
+    Hbc.load('local-caffeine').wont_be :installed?
+    Hbc.load('local-transmission').wont_be :installed?
+  end
+
+
   it "properly handles Casks that are not present" do
     lambda {
       shutup do
@@ -83,6 +93,10 @@ describe Hbc::CLI::Install do
 
     describe "with --force" do
       with_options.call(['--force'])
+    end
+
+    describe "with --skip-cask-deps" do
+      with_options.call(['--skip-cask-deps'])
     end
 
     describe "with an invalid option" do

dasbaumwolltier/homebrew-unsigned-casks

test/cask/cli/install_test.rb
@@ -42,6 +42,16 @@ describe Hbc::CLI::Install do
     }, %r{==> Success! local-transmission staged at '#{Hbc.caskroom}/local-transmission/2.61' \(0B\)})
   end
 
+  it "skips dependencies with --skip-cask-deps" do
+    shutup do
+      Hbc::CLI::Install.run('with-depends-on-cask-multiple', '--skip-cask-deps')
+    end
+    Hbc.load('with-depends-on-cask-multiple').must_be :installed?
+    Hbc.load('local-caffeine').wont_be :installed?
+    Hbc.load('local-transmission').wont_be :installed?
+  end
+
+
   it "properly handles Casks that are not present" do
     lambda {
       shutup do
@@ -83,6 +93,10 @@ describe Hbc::CLI::Install do
 
     describe "with --force" do
       with_options.call(['--force'])
+    end
+
+    describe "with --skip-cask-deps" do
+      with_options.call(['--skip-cask-deps'])
     end
 
     describe "with an invalid option" do