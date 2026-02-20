dasbaumwolltier/homebrew-unsigned-casks

commit 45ff854a0b5e02fe89b90896bfc26922acb9c5a2

iterm2 3.6.7

Casks/i/iterm2.rb
@@ -1,7 +1,7 @@
 cask "iterm2" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "3.6.6"
-  sha256 "68293d89ddf2140407879a651b42d9d05e12f403f69633ec635a96a29d90b4f3"
+  version "3.6.7"
+  sha256 "74f5590d776b640e42e67a2cfa3b7d021909fa9b6fc8732fd592f2d283602a91"
 
   url "https://iterm2.com/downloads/stable/iTerm2-#{version.dots_to_underscores}.zip"
   name "iTerm2"

commit 7c0c43ec0ebc574247eb163a6089692f0c119e9b

Update iterm2.rb - fix incorrect checksum
Introduced by https://github.com/Homebrew/homebrew-cask/commit/45ff854a0b5e02fe89b90896bfc26922acb9c5a2

Casks/i/iterm2.rb
@@ -1,7 +1,7 @@
 cask "iterm2" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
   version "3.6.7"
-  sha256 "74f5590d776b640e42e67a2cfa3b7d021909fa9b6fc8732fd592f2d283602a91"
+  sha256 "2572af26b69183f2341fc76b32405525deb6382e05c13bc0476731f5ae331928"
 
   url "https://iterm2.com/downloads/stable/iTerm2-#{version.dots_to_underscores}.zip"
   name "iTerm2"